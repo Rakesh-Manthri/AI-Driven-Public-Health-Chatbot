2ee0a1a569e6827b09d32efcc66d5b43d8655d2e
Prateek2007-cmd <chintawarprateek@gmail.com>
2025-09-18 09:27:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Portfolio from './pages/Portfolio';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Portfolio />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
