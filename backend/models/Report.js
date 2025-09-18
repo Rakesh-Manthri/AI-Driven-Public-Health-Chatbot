806a748c4ae4784459b2ffb817c401139d52696b
Prateek2007-cmd <chintawarprateek@gmail.com>
2025-09-18 09:22:01 +0530

    Update Report.js

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
+const ReportSchema = new mongoose.Schema({
+  userId: String,
+  filePath: String,
+  uploadedAt: {type:Date, default: Date.now}
+});
+module.exports = mongoose.model('Report',ReportSchema);
 
